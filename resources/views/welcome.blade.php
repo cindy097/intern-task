5897cd5a831834e2beca9e9517861f7df169d59b
cindy097 <cindychristie@gmail.com>
2025-07-02 12:32:15 +0700

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<!doctype html><html>  <head>    <meta charset="utf-8" />    <meta name="viewport" content="width=device-width, initial-scale=1.0" />    @vite('resources/css/app.css')  </head>  <body>  
+
+  <h1 class="text-3xl font-bold underline">      Hello world!    </h1>  
+ <div class="bg-white p-10 rounded-lg shadow-lg text-center">
+    <h1 class="text-3xl font-bold text-blue-600 mb-4">Tailwind Berhasil!</h1>
+    <p class="text-gray-700">Kalau kamu lihat ini dengan styling rapi, Tailwind-nya udah jalan 🎉</p>
+  </div>
+</body></html>
